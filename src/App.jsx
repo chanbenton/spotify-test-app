84b36afe70ea1e059950d439627e16315ed02dd0
Benton Chan <chan.benton+test@gmail.com>
2018-09-05 18:04:01 -0400

    Cleared src/ and re-initialized with starter code

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+	constructor (props){
+		super(props);
+		this.state = {
+			// state var's here
+		};
+    }
+    render() {
+        return (
+            <div className="App">
+                {/* // Content here */}
+            </div>
+        );
+    }
+}
+export default App;
